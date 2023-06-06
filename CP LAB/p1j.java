73bd61a4535146d278ce7736bd0fabb6747ca7b5
adithya3403 <a.dty.3403@gmail.com>
2023-06-06 23:54:36 +0530

    Update p1j.java

--- CP LAB/p1j.java
+++ CP LAB/p1j.java
@@ -1,7 +1,7 @@
 // j. Write a JAVA Program to find the lowest common ancestor of a binary tree
 
-// Given a binary tree, find the lowest common ancestor(LCA) of two given nodes in the tree. Given
-// the following binary tree:root=[3,5,1,6,2,0,8,null,null,7,4]
+// Given a binary tree, find the lowest common ancestor(LCA) of two given nodes in the tree.
+// Given the following binary tree:root=[3,5,1,6,2,0,8,null,null,7,4]
 
 // Example 1:
 // Input:root=[3,5,1,6,2,0,8,null,null,7,4],p=5,q=1
@@ -11,11 +11,24 @@
 // Example 2:
 // Input:root=[3,5,1,6,2,0,8,null,null,7,4],p=5,q=4
 // Output:5
-// Explanation:The LCA of nodes 5 and 4 is 5,since a node can be a descendant of itself according to
-// the LCA definition.Note:All of the nodes' values will be unique.p and q are different and both
+// Explanation:The LCA of nodes 5 and 4 is 5,since a node can be a descendant
+// of itself according to the LCA definition.
+
+// Note:All of the nodes' values will be unique. p and q are different and both
 // values will exist in the binary tree.
 
 import java.util.*;
+
+class TreeNode {
+    String val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(String val) {
+        this.val = val;
+    }
+}
+
 
 public class p1j {
     public static void main(String[] args) {
@@ -26,20 +39,20 @@ public class p1j {
         int i = 1;
         while (!queue.isEmpty() && i < elements.length) {
             TreeNode curr = queue.poll();
-            if (!elements[i].equals("null")) {
-                curr.left = new TreeNode(elements[i]);
-                queue.add(curr.left);
-            }
+            if (!elements[i].equals("null"))
+                queue.add(curr.left = new TreeNode(elements[i]));
             i++;
-            if (!elements[i].equals("null")) {
-                curr.right = new TreeNode(elements[i]);
-                queue.add(curr.right);
-            }
+            if (!elements[i].equals("null"))
+                queue.add(curr.right = new TreeNode(elements[i]));
             i++;
         }
         TreeNode p = new TreeNode("5");
         TreeNode q = new TreeNode("1");
         TreeNode lca = lowestCommonAncestor(root, p, q);
+        System.out.println(lca.val);
+        p = new TreeNode("5");
+        q = new TreeNode("4");
+        lca = lowestCommonAncestor(root, p, q);
         System.out.println(lca.val);
     }
 
@@ -51,24 +64,5 @@ public class p1j {
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         return (left != null && right != null) ? root : (left != null) ? left : right;
-    }
-}
-
-
-class TreeNode {
-    String val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {}
-
-    TreeNode(String val) {
-        this.val = val;
-    }
-
-    TreeNode(String val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
     }
 }
